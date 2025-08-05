7b6e0d3d019e7d9f43ace6f3136eceec38e41de6
Azhaguroobika-roobi <roobikaazhagu@gmail.com>
2025-08-05 14:15:43 +0530

    portfolio changed

--- src/components/home.js
+++ src/components/home.js
@@ -8,7 +8,7 @@ function Home() {
     return(
          <Box sx={{ height: '90vh', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column',background: 'linear-gradient(135deg, #ae917bff 0%, #705640ff 100%)',color: 'white' }}>
       <Typography variant="h2">Hello, I'm Azhagu Roobika</Typography>
-      <Typography variant="h5" sx={{ mt: 2 }}>Frontend Developer | React | .NET Enthusiast</Typography>
+      <Typography variant="h5" sx={{ mt: 2 }}>Frontend Developer | React</Typography>
     </Box>
     );
 }
